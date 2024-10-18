7fda6140bc304ab74f47221c65b2314e27162eda
rn-arg <reneg.noventa@gmail.com>
2024-10-18 19:23:42 -0300

    Base

---
+++ blogG15/blogG15/configuraciones/local.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
